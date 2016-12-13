6408bc3ca8fa38f57afde30d2fa137c070c5afaf
Rafael Tovar <hola@rafaeltovar.info>
2016-12-13 15:32:35 +0100

    Set buttons options and ngModel

--- dist/angular-yellowtext.js
+++ dist/angular-yellowtext.js
@@ -1,10 +1,68 @@
 angular.module("Angular-YellowText", [])
-.directive("ngYellowtext",function(){
+.directive("ngYellowtext", ['$timeout', function($timeout){
     return {
         restrict: 'A',
         require: 'ngModel',
         link: function(scope, element, attrs, ngModel) {
-            element.YellowText();
-        }
+            console.debug(attrs, ngModel);
+
+            var ytClass = 'js-yellowtext-' + Math.random().toString(36).substring(7);
+            // http://stackoverflow.com/questions/14687822/how-to-pass-a-json-as-a-string-param-to-a-directive
+            var buttons = eval('(' + attrs.ngYellowtext + ')');
+
+			var options = { defaultActions: buttons,
+                            iFrameClass: ytClass,
+                            isContentChanged: function(changed){
+                                var value = getContentFromEditor();
+                                console.debug("passa", value);
+                                updateModel.call(this, value);
+                            }
+                            };
+
+
+            getContentFromEditor = function() {
+                return editor.contents().find("body").html();
+            }
+
+            setContentToEditor = function(content) {
+                editor.contents().find("body").append( content );
+            }
+
+            // Expose scope var with loaded state of Redactor
+            var editorLoaded = false;
+
+            var updateModel = function updateModel(value) {
+                // $timeout to avoid $digest collision
+                $timeout(function() {
+                    scope.$apply(function() {
+                        ngModel.$setViewValue(value);
+                    });
+                });
+            },
+            editor;
+
+            // put in timeout to avoid $digest collision.  call render() to
+            // set the initial value.
+            $timeout(function() {
+                //editor = element.redactor(options);
+                element.YellowText(options);
+                editor = angular.element("." + ytClass);
+                ngModel.$render();
+                /*element.on('remove',function(){
+                    element.off('remove');
+                    //element.redactor('core.destroy');
+                });*/
+            });
+
+            ngModel.$render = function() {
+                if(angular.isDefined(editor)) {
+                    $timeout(function() {
+                        setContentToEditor(ngModel.$viewValue || '' );
+                        editorLoaded = true;
+                    });
+                }
+            };
+
+        } // end link
     };
-});
+}]);
